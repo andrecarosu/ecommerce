61a8dc9d89dfe3640c154944bf4afedde46a6639
andrecarosu <andrecarosu@hotmail.com>
2023-04-26 10:39:13 -0500

    eliminación de archivos no necesarios

--- api/src/routes/allProducts.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express");
-const {postAllProductsHandler} = require('../handlers/productos/postAllProducts');
-
-
-const router = Router();
-
-router.post("/",postAllProductsHandler);
-
-module.exports = router;
