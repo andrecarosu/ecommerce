f1e50ecb22d7a982ddee99ea0be8670e58850b3e
FedeMas1 <javierjazmin@gmail.com>
2023-05-01 16:44:56 +0100

    Rutas de email corregidas

--- api/src/routes/email.js
+++ api/src/routes/email.js
@@ -2,8 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const { getUserByEmailHandler, getCommerceByEmailHandler } = require("../handlers/email/handlerGetEmail");
 
-
 router.get("/", getUserByEmailHandler)
-router.get("/commerce", getCommerceByEmailHandler)
+
 
 module.exports = router;
