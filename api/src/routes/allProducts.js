a4a988d1e12b824dd5691b6667322290550a93b9
andrecarosu <andrecarosu@hotmail.com>
2023-05-04 12:48:23 -0500

    armado de front para deploy

--- api/src/routes/allProducts.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express");
-const {postAllProductsHandler} = require('../handlers/product/postAllProducts');
-
-
-const router = Router();
-
-router.post("/",postAllProductsHandler);
-
-module.exports = router;
--- api/src/routes/email.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const { getUserByEmailHandler, getCommerceByEmailHandler } = require("../handlers/email/handlerGetEmail");
-
-router.get("/", getUserByEmailHandler)
-
-
-module.exports = router;
