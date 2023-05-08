64cf2ce6d47211ff8b0a5019f21ce16b331f3c88
andrecarosu <andrecarosu@hotmail.com>
2023-05-08 02:35:08 -0500

    arreglo de  url

--- api/src/app.js
+++ api/src/app.js
@@ -37,7 +37,7 @@ server.use(morgan('dev'));
 // });
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ecommerce-five-green.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://ecommerce-khaki-nine.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
