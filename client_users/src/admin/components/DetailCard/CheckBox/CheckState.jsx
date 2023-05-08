bce3e3ea868b879ce5a012a591ed7ee1f15da844
andrecarosu <andrecarosu@hotmail.com>
2023-05-08 12:13:40 -0500

    adicion de url

--- client_users/src/admin/components/DetailCard/CheckBox/CheckState.jsx
+++ client_users/src/admin/components/DetailCard/CheckBox/CheckState.jsx
@@ -12,7 +12,7 @@ const CheckState = ({ product_id, state }) => {
 
     useEffect(async () => {
         try {
-            await axios.put(`https://ecommerce-khaki-nine.vercel.app/products/${product_id}`, { state: check });
+            await axios.put(`https://deploynodejsecommerce.onrender.com/products/${product_id}`, { state: check });
         } catch (error) {
             console.error(error)
         }
