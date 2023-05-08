80905aeceb41d276143b82ad382b5728698432be
andrecarosu <andrecarosu@hotmail.com>
2023-05-08 04:04:29 -0500

    arreglo de carpetas

--- client_users/src/pages/shopping/user.js
+++
@@ -1,9 +0,0 @@
-import Cookies from "js-cookie";
-import jwt_decode from "jwt-decode";
-
-export const mail = () => {
-    const token = Cookies.get("user_token");
-    const decodedToken = jwt_decode(token);
-    const email = decodedToken.email;
-    return email;
-}
