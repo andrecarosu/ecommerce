3592e33af1ec080613283d046b9f14f454f9b111
andrecarosu <andrecarosu@hotmail.com>
2023-05-09 10:59:55 -0500

    arreglo en detail para reviews

--- client_users/src/pages/detail/Detail.jsx
+++ client_users/src/pages/detail/Detail.jsx
@@ -12,10 +12,11 @@ import Loader from '../../components/loader/loader'
 // import axios from 'axios'
 
 
-const Detail = () => {
+const Detail = (props) => {
 
   const { product, carrito, countCarrito, display } = useSelector(state => state)
   console.log(product);
+  const  product_id  = props.id
   const { id } = useParams()
   const dispatch = useDispatch();
   const estaLogueado = localStorage.getItem("estaLogueado");
@@ -23,8 +24,8 @@ const Detail = () => {
   // const URL = 'http://localhost:3001'
 
   useEffect(() => {
-    dispatch(getProductById(id))
-    dispatch(getReviews(id))
+    dispatch(getProductById(product_id?  product_id: id ))
+    dispatch(getReviews(product_id?  product_id: id))
     window.localStorage.setItem("carrito", JSON.stringify(carrito));
     window.localStorage.setItem("count", JSON.stringify(countCarrito));
     return (() => {
