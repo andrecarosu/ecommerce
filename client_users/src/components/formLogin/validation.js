555bc96ba1c137c4072d7b57b9240f10b914f976
andrecarosu <andrecarosu@hotmail.com>
2023-08-27 10:52:39 -0500

    arreglo en login validations

--- client_users/src/components/formLogin/validation.js
+++ client_users/src/components/formLogin/validation.js
@@ -1,12 +1,12 @@
 export default function validation(values) {
     let errors = {}
 
-    if(!values.email) {
+    if(values.email &&!values.email) {
         errors.email = 'Ingrese un email'
     } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/.test(values.email)){
         errors.email = 'Ingrese un email válido'
     }
-    if(!values.password) errors.password = 'Ingrese una contraseña'
+    if(values.password&&!values.password) errors.password = 'Ingrese una contraseña'
     console.log(errors.email, values.email);
     return errors
 }
